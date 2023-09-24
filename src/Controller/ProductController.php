5aba4e6082a1f06245b2181902d5ac17226b3359
Piotr Puchacz <Piotr.Puchacz@silkycoders.com>
2023-09-24 10:17:42 +0200

    Add product addition and editing

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,8 +5,10 @@ namespace App\Controller;
 use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\SerializerInterface;
@@ -19,15 +21,58 @@ class ProductController extends AbstractController
         private SerializerInterface $serializer,
     ) {}
 
-    #[Route('/', name: 'list')]
-    public function get(): Response
+    #[Route('/', name: 'list', methods: [Request::METHOD_GET])]
+    public function get(#[MapQueryParameter] int $page = 1, #[MapQueryParameter] int $limit = 10): Response
     {
-//        $page = $this->request->get('page');
-//        $limit = $this->request->get('limit');
+        if ($page < 1) {
+            return new JsonResponse("Invalid PAGE query parameter - should be greater than 0");
+        }
 
-        $products = $this->entityManager->getRepository(Product::class)->findAll();
+        if ($limit < 1) {
+            return new JsonResponse("Invalid LIMIT query parameter - should be greater than 0");
+        }
+
+        $offset = ($page - 1) * $limit;
+        $products = $this->entityManager->getRepository(Product::class)->findBy([], null, $limit, $offset);
         $json = $this->serializer->serialize($products, JsonEncoder::FORMAT);
 
         return new Response($json);
+    }
+
+    #[Route('/', name: 'add', methods: [Request::METHOD_POST])]
+    public function add(Request $request): Response
+    {
+        $json = $request->getContent();
+        $products = $this->serializer->deserialize($json, Product::class . '[]', 'json');
+
+        foreach ($products as $product) {
+            $this->entityManager->persist($product);
+        }
+
+        $this->entityManager->flush();
+        return new JsonResponse();
+    }
+
+    #[Route('/{id}', name: 'edit', methods: [Request::METHOD_PUT])]
+    public function edit(Request $request, int $id): Response
+    {
+        $productInDB = $this->entityManager->getRepository(Product::class)->findOneBy(['id' => $id]);
+        if ($productInDB === null) {
+            return new Response("Product with id $id not found", Response::HTTP_NOT_FOUND);
+        }
+
+        $json = $request->getContent();
+        $product = $this->serializer->deserialize($json, Product::class, 'json');
+
+        $productInDB->name = $product->name;
+        $productInDB->description = $product->description;
+        $productInDB->price = $product->price;
+        $productInDB->brand = $product->brand;
+        $productInDB->max_quantity = $product->max_quantity;
+        $productInDB->picture = $product->picture;
+
+        $this->entityManager->persist($product);
+        $this->entityManager->flush();
+        return new JsonResponse();
     }
 }
